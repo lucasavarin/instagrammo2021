71d6275b85350ead6115a76dec40300cc63a07db
Mijo <mimy291628@gmail.com>
2021-02-01 18:00:16 +0100

    add network-security

---
+++ app/src/main/java/com/example/instagrammo/HomeActivity.kt
@@ -0,0 +1,4 @@
+package com.example.instagrammo
+
+class HomeActivity {
+}
---
+++ app/src/main/java/com/example/instagrammo/LoginActivity.kt
@@ -0,0 +1,41 @@
+package com.example.instagrammo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import network.ApiClient
+import request.AuthRequest
+import response.AuthResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.android.synthetic.main.
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+    }
+    fun accedi(){
+        val authRequest = AuthRequest(
+            username = username.text.toString(),
+            password = password.text.toString()
+        )
+        ApiClient.getClient.doAuth(authRequest).enqueue(object : Callback<AuthResponse> {
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                println("hai fallito-----------> " + t.message)
+            }
+
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                val intent= Intent(context,HomeActivity::class.java)
+                startActivity(intent)
+            }
+
+        })
+    }
+}
