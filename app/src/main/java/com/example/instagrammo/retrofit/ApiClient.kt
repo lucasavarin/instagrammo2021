817f45c1e71d3b5ef9bbab8f1ca898de48957518
Filippo <filippo.bot@gmail.com>
2021-02-01 17:19:06 +0100

    Implementazione LogIn

--- app/src/main/java/com/example/instagrammo/retrofit/ApiClient.kt
+++ app/src/main/java/com/example/instagrammo/retrofit/ApiClient.kt
@@ -8,7 +8,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    const val BASEURL : String = ""
+    const val BASEURL : String = "http://www.nbarresi.it/"
     val GetClient : ApiInterface
     get(){
         val interceptor = HttpLoggingInterceptor()
--- app/src/main/java/com/example/instagrammo/view/LogIn.kt
+++ app/src/main/java/com/example/instagrammo/view/LogIn.kt
@@ -1,8 +1,10 @@
-package com.example.instagrammo
+package com.example.instagrammo.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import com.example.instagrammo.R
 import com.example.instagrammo.retrofit.ApiClient
 import com.example.instagrammo.retrofit.AuthRequest
 import com.example.instagrammo.retrofit.AuthResponse
@@ -11,7 +13,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -22,18 +24,18 @@ class MainActivity : AppCompatActivity() {
         val request = AuthRequest(username, password)
 
         btnAccess.setOnClickListener {
-            Toast.makeText(this@MainActivity, "il mio username:" + username + "la mia password: " + password, Toast.LENGTH_LONG).show()
+            Toast.makeText(this@LogIn, "Bottone premuto", Toast.LENGTH_LONG).show()
             ApiClient.GetClient.doAuth(request).enqueue(object : Callback<AuthResponse> {
 
                 override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
-                    print("Login Fallito " + t)
+                    Toast.makeText(this@LogIn, "LogIn fallito", Toast.LENGTH_LONG).show()
                 }
 
                 override fun onResponse(
                     call: Call<AuthResponse>,
                     response: Response<AuthResponse>
                 ) {
-                    Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_LONG).show()
+                    Toast.makeText(this@LogIn, response.message(), Toast.LENGTH_LONG).show()
                 }
 
             })
