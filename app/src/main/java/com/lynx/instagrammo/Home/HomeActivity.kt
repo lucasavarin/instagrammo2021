ecc1b02324e3d0622efa49590f90ac7f2af8769c
Daniele  Ferrillo <danyferrillo@gmail.com>
2021-02-01 18:05:56 +0100

    Fix HomeActivity

--- app/src/main/java/com/lynx/instagrammo/Home/HomeActivity.kt
+++ app/src/main/java/com/lynx/instagrammo/Home/HomeActivity.kt
@@ -1,7 +1,6 @@
-package com.lynx.instagrammo.HomePage
+package com.lynx.instagrammo.Home
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lynx.instagrammo.R
 
