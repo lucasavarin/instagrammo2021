f5cc44f06c63a64a22f9dcd1ce58dc66ac1ab2a7
RoccoDB <rocco.dibiase@hotmail.it>
2021-02-01 17:26:44 +0100

    Connection API

--- app/src/main/java/com/lynx/instagrammo/MainActivity.kt
+++ app/src/main/java/com/lynx/instagrammo/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.lynx.instagrammo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lynx.instagrammo.API.ApiClient
+import com.lynx.instagrammo.HomePage.HomeActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            doLogin()
+        }
+
     }
+
+    fun doLogin() {
+        val authRequest = AuthRequest(
+            username = editTextUsername.text.toString(),
+            password = editTextPassword.text.toString()
+        )
+        ApiClient.getClient.doAuth(authRequest).enqueue(object : Callback<AuthResponse>
+        {
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                println("Error : ${t.message}" )
+            }
+
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+    }
+
+
 }
