e49c876efeebb4bf4bb415cc0868dcbbac91f4ad
DESKTOP\jhero <jherome.samson@lynxspa.com>
2021-02-01 12:01:54 +0100

    primo push

---
+++ components/src/main/java/com/example/components/buttons/ConfirmButtonComponent.kt
@@ -0,0 +1,15 @@
+package com.example.components.buttons
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+import com.example.components.R
+
+class ConfirmButtonComponent @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatButton(context, attrs, R.attr.confirm_button_style) {
+
+
+}
