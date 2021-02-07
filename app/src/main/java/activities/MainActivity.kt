d5ae46fd7be7616abdf14ff8b7dc289bcf4c9935
Federico <dm9000@libero.it>
2021-02-07 17:42:45 +0100

    created classes Prefs, BaseApp and HomeActivity

--- app/src/main/java/activities/MainActivity.kt
+++ app/src/main/java/activities/MainActivity.kt
@@ -1,40 +1,39 @@
 package activities
 
 import API.ApiClient
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.instagrammo.R
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.POST
 import util.AuthRequest
 import util.AuthResponse
+import util.prefs
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*fun authRequest = AuthRequest (
-                username = editTextUsername.text.toString(),
-                password = editTextPassword.text.toString()
-                )
-        ApiClient.getClient.doAuth(authRequest()).enqueue(object : Callback<AuthResponse>
-        {
-            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
-                println("Error: ${t.message}")
-            }
+        val request = AuthRequest("lsavarin", "lucasava")
 
-            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
-                val intent = Intent(this@MainActivity, MainActivity::class.java)
-                startActivity(intent)
-            }
-        })
-        */
+        /*login.setOnClickListener {
+            ApiClient.GetClient.doAuth(request).enqueue(object : Callback<AuthResponse> {
+
+                override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                    print("Login Fallito")
+                }
+
+                override fun onResponse(
+                        call: Call<AuthResponse>,
+                        response: Response<AuthResponse>
+                ) {
+                    if(!response.body()?.authToken.isNullOrEmpty()){
+                        prefs.rememberPassword = response.body()?.authToken.toString();
+                    }
+                }
+            })
+        }*/
     }
 }
