882166bdd0a4ac03a7e8a02be30c680908fa7783
Marcos Costa <marcos300896@gmail.com>
2021-02-09 11:55:00 +0100

    adapter and viewHolder of PostRecycleView

---
+++ app/src/main/java/com/costa/adapter/PostAdapter.kt
@@ -0,0 +1,39 @@
+package com.costa.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.costa.beans.PostOut
+import com.costa.instagrammo.R
+
+class PostAdapter (private val posts: List<PostOut>) :
+    RecyclerView.Adapter<PostViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.post_item, parent, false)
+       // val view =parent.inflate(R.layout.post_item, parent, false)
+
+        return PostViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+
+       // holder.bindPost(posts.get(position))
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = posts.size
+
+
+}
---
+++ app/src/main/java/com/costa/adapter/PostViewHolder.kt
@@ -0,0 +1,13 @@
+package com.costa.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+
+    init {
+        // Define click listener for the ViewHolder's View.
+
+    }
+}
