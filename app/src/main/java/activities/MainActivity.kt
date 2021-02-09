d27eb6bfadb39bc061f942e1cdba0094dd69fbda
Federico <dm9000@libero.it>
2021-02-09 10:34:04 +0100

    fixed class MainActivity, created Intent

--- app/src/main/java/activities/MainActivity.kt
+++ app/src/main/java/activities/MainActivity.kt
@@ -1,9 +1,11 @@
 package activities
 
 import API.ApiClient
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instagrammo.R
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,10 +18,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val username = username.text
+        val password = password.text
+
         val request = AuthRequest("lsavarin", "lucasava")
 
-        /*login.setOnClickListener {
-            ApiClient.GetClient.doAuth(request).enqueue(object : Callback<AuthResponse> {
+        btn_login.setOnClickListener {
+            ApiClient.getClient.doAuth(request).enqueue(object : Callback<AuthResponse> {
 
                 override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
                     print("Login Fallito")
@@ -34,6 +39,10 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             })
-        }*/
+        }
+        val intent = Intent(applicationContext, HomeActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("chiave", "valore")
+        startActivity(intent, bundle)
     }
 }
