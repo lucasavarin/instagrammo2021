8d83ff2ca4429c6f218a55a6f60a74c117fcc0a9
Dalton Calderon <itscalde@hotmail.com>
2021-02-13 16:14:20 +0100

    Fix nav follow / fixed logo

--- app/src/main/java/com/costa/instagrammo/HomeActivity.kt
+++ app/src/main/java/com/costa/instagrammo/HomeActivity.kt
@@ -8,25 +8,37 @@ import com.costa.adapter.PostAdapter
 import com.costa.views.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.follow_item.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        //bottom_nav.background = null
+
+        bottom_nav.setOnClickListener{
+            bottom_nav.isSelected = !bottom_nav.isSelected
+        }
+
+        /*nomeUtente.setOnClickListener{
+            nomeUtente.isSelected = true
+        }
+        */
+
+        /*
+        bottom_nav.background = null
 
         //followers
+        val posts:ArrayList<String> = ArrayList()
+        for (i in 1..10) {
 
-        //val posts:ArrayList<String> = ArrayList()
-
-        //for (i in 1..10) {
-
-        //    posts.add("Utente # $i")
-        //}
+            posts.add("Utente # $i")
+        }
 
         //recycler_view_orizzontale.layoutManager = LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false)
         //recycler_view_orizzontale.adapter = PostAdapter(posts)
+
+        */
 
 
         lateinit var homeFragment: HomeFragment
