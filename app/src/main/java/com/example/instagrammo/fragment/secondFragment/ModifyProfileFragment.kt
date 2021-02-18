df811040b9e8d88a6f3895b391e3254b230ee57e
MIJO <gipsymasnaki@gmail.com>
2021-02-18 15:13:40 +0100

    fix modify profile

--- app/src/main/java/com/example/instagrammo/fragment/secondFragment/ModifyProfileFragment.kt
+++ app/src/main/java/com/example/instagrammo/fragment/secondFragment/ModifyProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.example.instagrammo.fragment.secondFragment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.instagrammo.R
+import com.example.instagrammo.fragment.ProfileFragment
+import kotlinx.android.synthetic.main.fragment_modify_profile.*
 
 
 class ModifyProfileFragment : Fragment() {
@@ -19,6 +23,12 @@ class ModifyProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_modify_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_back.setOnClickListener {
+            
+        }
+    }
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
