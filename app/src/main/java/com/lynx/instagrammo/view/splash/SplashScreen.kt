d9ee48cbe70efaa2a11943add638dd0f13a27dfd
Daniele  Ferrillo <danyferrillo@gmail.com>
2021-02-18 16:06:51 +0100

    add splash screen activity

---
+++ app/src/main/java/com/lynx/instagrammo/view/splash/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.lynx.instagrammo.view.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.lynx.instagrammo.R
+import com.lynx.instagrammo.view.login.LoginActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
