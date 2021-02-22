aadea36fa6c29c4ac23897ffb1621c67bcbe047c
Mijo <mimy291628@gmail.com>
2021-02-22 11:03:47 +0100

    fix customViewController

--- app/src/main/java/com/example/instagrammo/view/CustomViewController.kt
+++ app/src/main/java/com/example/instagrammo/view/CustomViewController.kt
@@ -2,33 +2,33 @@ package com.example.instagrammo.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.ActionMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.instagrammo.R
-
-/*
-class HeaderCustomView : ConstraintLayout{
-    constructor(context: Context, attrs: AttributeSet) : super(context,attrs){
-        inflate(context, R.layout.custom_view_header, this)
-    }
-}
-class CustomViewController @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+import kotlinx.android.synthetic.main.custom_view_controller.view.*
 
 
-}
-class CustomViewController : ConstraintLayout{
-    constructor(context : Context,attrs : AttributeSet?) : super(context,attrs){
-        View.inflate(context,R.layout.custom_view_controller,this)
+class CustomViewController (context : Context ,attrs : AttributeSet) : ConstraintLayout (context,attrs) {
+
+     private val onBackPressed : ArrayList<(() -> Unit)> = arrayListOf()
+
+    init {
+        inflate(context,R.layout.custom_view_controller,this)
+
+        val attributes = context.obtainStyledAttributes(attrs,R.styleable.CustomViewController)
+        btn_back.setOnClickListener { onBackPressed.forEach{ it.invoke()} }
+        modify_title.text = attributes.getString(R.styleable.CustomViewController_title)
+        attributes.recycle()
     }
- */
-class CustomViewController : ConstraintLayout {
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        View.inflate(context, R.layout.custom_view_controller, this)
+
+    fun setOnBackPressedListner(callback: () -> Unit){
+        onBackPressed.add(callback)
     }
+
+
 }
 
 
