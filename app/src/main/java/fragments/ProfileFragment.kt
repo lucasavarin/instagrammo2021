575a882c825ebc229c4309bf2f5550515b71fd7d
lorenzo <lorenzo.vassalle07@gmail.com>
2021-02-27 11:22:43 +0100

    profile recycler views implementation

--- app/src/main/java/fragments/ProfileFragment.kt
+++ app/src/main/java/fragments/ProfileFragment.kt
@@ -1,15 +1,27 @@
 package fragments
 
+import adapters.PostsAdapter
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import bean.rest.PostResponse
 import com.example.instagrammo.R
 import interfaces.ButtonEditProfileListener
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import utils.api.ApiClient
+import utils.extensions.prefs
 import java.util.*
 
 class ProfileFragment : Fragment() {
@@ -27,6 +39,7 @@ class ProfileFragment : Fragment() {
         editButton.setOnClickListener {
             listenerButtonEdit?.onClickListenerEditButtonProfile(true)
         }
+        callPostProfile()
     }
 
     companion object {
@@ -38,6 +51,36 @@ class ProfileFragment : Fragment() {
         if (context is ButtonEditProfileListener) {
             listenerButtonEdit = context
         }
+    }
+
+    fun callPostProfile(){
+        ApiClient.getClient.getPosts(prefs.profileId!!).enqueue(object: Callback<PostResponse> {
+            override fun onResponse(
+                call: Call<PostResponse>,
+                response: Response<PostResponse>
+            ) {
+                val gridLayoutManager = GridLayoutManager(context, 3)
+                gridRecyclerView.layoutManager = gridLayoutManager
+                gridRecyclerView.adapter = PostsAdapter(response.body()!!.payload!!)
+                gridImageButton.setOnClickListener{
+                    gridRecyclerView.setVisibility(View.VISIBLE)
+                    monoRecyclerView.setVisibility(View.INVISIBLE)
+                }
+
+                val linearLayoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+                monoRecyclerView.layoutManager = linearLayoutManager
+                monoRecyclerView.adapter = PostsAdapter(response.body()!!.payload!!)
+                monoImageButton.setOnClickListener{
+                    gridRecyclerView.setVisibility(View.INVISIBLE)
+                    monoRecyclerView.setVisibility(View.VISIBLE)
+                }
+
+                Log.i("info", response.body()!!.result.toString())
+            }
+            override fun onFailure(call: Call<PostResponse>, t: Throwable) {
+                println("Chiamata non eseguita!!!! $t")
+            }
+        })
     }
 
 }
